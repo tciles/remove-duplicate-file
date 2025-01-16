bd5e2a8d3ce5bb418da9d503464d6ca15f6488f1
Thomas CILES <thomasciles@gmail.com>
2025-01-16 22:22:28 +0100

    feat(listing) : move duplicate files

--- Program.cs
+++ Program.cs
@@ -23,6 +23,8 @@ if (!Directory.Exists(path)) {
     return;
 }
 
+string outputPath = $@"{path}/_duplicate_";
+
 string[] fileEntries = Directory.GetFiles(args[0]);
 Array.Sort(fileEntries);
 int len = fileEntries.Length;
@@ -30,11 +32,30 @@ int i;
 
 Console.WriteLine($"Analyse {len} files in {path}");
 
+string[] hashes = new string[len];
+
 for (i = 0; i < len; i++)
 {
     string fileName = fileEntries[i];
     FileInfo fileInfo = new FileInfo(fileName);
     string hash = GetFileHash(fileName);
 
-    Console.WriteLine($"{fileInfo.Name} - {fileInfo.Length} - {hash}");
+    foreach (string h in hashes)
+    {
+        if (hash.Equals(h)) {
+
+            if (!Directory.Exists(outputPath)) {
+                Directory.CreateDirectory(outputPath);
+            }
+            
+            fileInfo.MoveTo($@"{outputPath}/{fileInfo.Name}");
+            Console.WriteLine($"Move file {fileName}");
+
+            break;
+        }
+    }
+
+    hashes[i] = hash;
+
+    // Console.WriteLine($"{fileInfo.Name} - {fileInfo.Length} - {hash}");
 }
